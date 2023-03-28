7e502d3e3ddaf42cf95e42afe4efc45e80122e08
Drake Evans <31104161+DrakeEvans@users.noreply.github.com>
2023-03-28 12:49:40 -0400

    feat: Add tooling config files

--- script/DeployCounter.s.sol
+++ script/DeployCounter.s.sol
@@ -3,13 +3,19 @@ pragma solidity ^0.8.19;
 
 import { BaseScript } from "frax-std/BaseScript.sol";
 import { console } from "frax-std/FraxTest.sol";
-import { deployCounter } from "./deployFunctions.sol";
+import { Counter } from "../src/Counter.sol";
+
+// This is a free function that can be imported and used in tests or other scripts
+function deployCounter() returns (address _address){
+  Counter _counter = new Counter();
+  _address = address(_counter);
+}
 
 // Run this with source .env && forge script --broadcast --rpc-url $MAINNET_URL DeployCounter.s.sol
-
 contract DeployCounter is BaseScript {
   function run() public broadcaster {
     address _address = deployCounter();
     console.log("Deployed Counter at address: ", _address);
   }
 }
+
