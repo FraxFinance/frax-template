5ad9af342a445e5ee6c3d4aa92fc5c2e70a804a3
Thomas Clement <88335455+tom2o17@users.noreply.github.com>
2023-12-13 14:40:57 -0500

    Fix lint - parser resolution, run in CI

--- src/script/DeployCounter.s.sol
+++ src/script/DeployCounter.s.sol
@@ -6,16 +6,15 @@ import { console } from "frax-std/FraxTest.sol";
 import { Counter } from "../contracts/Counter.sol";
 
 // This is a free function that can be imported and used in tests or other scripts
-function deployCounter() returns (address _address){
-  Counter _counter = new Counter();
-  _address = address(_counter);
+function deployCounter() returns (address _address) {
+    Counter _counter = new Counter();
+    _address = address(_counter);
 }
 
 // Run this with source .env && forge script --broadcast --rpc-url $MAINNET_URL DeployCounter.s.sol
 contract DeployCounter is BaseScript {
-  function run() public broadcaster {
-    address _address = deployCounter();
-    console.log("Deployed Counter at address: ", _address);
-  }
+    function run() public broadcaster {
+        address _address = deployCounter();
+        console.log("Deployed Counter at address: ", _address);
+    }
 }
-
