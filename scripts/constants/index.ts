d5466d99dae23c6a7d46d6d4988acaa67fcb1c88
Carter Carlson <pegahcarter@gmail.com>
2024-02-19 11:59:36 -0800

    ops: basic fraxchain constants

--- scripts/constants/index.ts
+++ scripts/constants/index.ts
@@ -1,1 +1,7 @@
 export * as Mainnet from "./mainnet";
+export * as Holesky from "./holesky";
+export * as FraxchainDeployment from "./fraxchain-deployments";
+export * as FraxchainL1Devnet from "./fraxchain-devnet-l1";
+export * as FraxchainL2Devnet from "./fraxchain-devnet-l2";
+export * as FraxchainTestnet from "./fraxchain-testnet";
+export * as FraxchainMainnet from "./fraxchain-mainnet";
