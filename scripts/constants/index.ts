99125950d5554ddfafab9cf68afe881a0d55f340
Carter Carlson <pegahcarter@gmail.com>
2024-02-21 13:10:19 -0800

    docs: proxy and comptroller addresses

--- scripts/constants/index.ts
+++ scripts/constants/index.ts
@@ -4,4 +4,5 @@ export * as FraxtalDeployment from "./fraxtal-deployments";
 export * as FraxtalL1Devnet from "./fraxtal-devnet-l1";
 export * as FraxtalL2Devnet from "./fraxtal-devnet-l2";
 export * as FraxtalTestnet from "./fraxtal-testnet";
-export * as FraxtalMainnet from "./fraxchain-mainnet";
+export * as FraxtalMainnet from "./fraxtal-mainnet";
+export * as FraxtalStandardProxies from "./fraxtal-standard-proxies";
