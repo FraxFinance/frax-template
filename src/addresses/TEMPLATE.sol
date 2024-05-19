64d8a343d9f61888518ba8121b55b99003f62d1e
travis <travis@everipedia.com>
2024-05-19 15:38:56 -0700

    Address list 6

---
+++ src/addresses/TEMPLATE.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: ISC
+pragma solidity >=0.8.0;
+
+// **NOTE** Generated code, do not modify.  Run 'npm run generate:constants'.
+
+import { TestBase } from "forge-std/Test.sol";
+
+library Canonicals {
+    address internal constant FRAX = aaaa;
+    address internal constant FXS = aaaa;
+    address internal constant FPI = aaaa;
+    address internal constant FPIS = aaaa;
+    address internal constant FRXETH = aaaa;
+    address internal constant SFRXETH = aaaa;
+    address internal constant SFRAX = aaaa;
+}
+
+library Farms {
+    address internal constant aaaa;
+}
+
+library PoolRelatedTokens {
+    address internal constant aaaa;
+}
+
+library FraxFerry {
+    address internal constant aaaa;
+}
+
+library FraxBonds {
+    address internal constant aaaa;
+}
+
+library Fraxlend {
+    address internal constant aaaa;
+}
+
+library Fraxswap {
+    address internal constant aaaa;
+}
+
+library Misc {
+    address internal constant aaaa;
+}
+
+library Multisigs {
+    address internal constant aaaa;
+}
+
+library Oracles {
+    address internal constant aaaa;
+}
